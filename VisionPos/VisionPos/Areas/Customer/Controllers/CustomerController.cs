9671606146c206332b5a39be1533032b8facb7cd
HamzaVision <hamza@visionplus.com.pk>
2023-07-20 17:59:59 +0500

    Customer Create

--- VisionPos/VisionPos/Areas/Customer/Controllers/CustomerController.cs
+++ VisionPos/VisionPos/Areas/Customer/Controllers/CustomerController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using VisionPos.Data;
+using VisionPos.Models.ViewModels;
 
 namespace VisionPos.Areas.Customer.Controllers
 {
@@ -13,7 +16,48 @@ namespace VisionPos.Areas.Customer.Controllers
         }
         public IActionResult Index()
         {
-            return View(_db.Customers.ToList());
+            return View(_db.Customers.Include(x => x.CustomerTypes).ToList());
+        }
+
+        public IActionResult Create()
+        {
+
+            CustomerCustomerTypeListViewModel mod = new CustomerCustomerTypeListViewModel()
+            {
+                Customer = new Models.Customer(),
+                CustomersList = _db.CustomerTypes.Select(a => new SelectListItem()
+                { Text = a.CustomerType, Value = a.Id.ToString() }).ToList(),
+                //CustomersNameList = _db.Customers.Select(a => new SelectListItem()
+                //{ Text = a.Name, Value = a.Id.ToString() }).ToList()
+
+            };
+
+            return View("CreateAndEdit", mod);
+
+        }
+
+
+
+        public IActionResult Edit(int id)
+        {
+            var cus = _db.CustomerTypes.FirstOrDefault(x => x.Id == id);
+            return View("CreateAndEdit", cus);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CustomerCustomerTypeListViewModel obj)
+        {
+            var cus = _db.Customers.Where(x => x.Name == obj.Customer.Name);
+            if (cus != null)
+            {
+                return View("CreateAndEdit", obj);
+            }
+
+            obj.Customer.CreationDate = DateTime.Now;
+            _db.Customers.Add(obj.Customer);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ VisionPos/VisionPos/Models/ViewModels/CustomerCustomerTypeListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace VisionPos.Models.ViewModels
+{
+    public class CustomerCustomerTypeListViewModel
+    {
+        public Customer Customer { get; set; }
+        public List<SelectListItem> CustomersList { get; set; }
+        //public List<SelectListItem> CustomersNameList { get; set; }
+    }
+}
